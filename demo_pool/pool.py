3cd9277537f3ba378021e262bd87b72ed124400a
Bali Hsu <balihsu@accudata.com.tw>
2022-09-07 17:15:08 +0800

    clean pool.py for share

--- demo_pool/pool.py
+++ demo_pool/pool.py
@@ -3,7 +3,7 @@ import time
   
 def Foo(i):
     print('foo',i)
-    time.sleep(0.5)
+    time.sleep(3.5)
     return i+100
   
 def Bar(arg):
@@ -12,16 +12,10 @@ def Bar(arg):
 
 if __name__ == '__main__':
     with Pool(processes=5) as pool:
-        #rs = []
         for i in range(10):
             print('i: ', i)
             r = pool.apply_async(func=Foo, args=(i,),callback=Bar)
-        #    rs.append(r)
-            #pool.apply(func=Foo, args=(i,))
-        #for r in rs:
-        #    r.wait()
-        #    print(f"get {r.get()}")
         print('after apply_async, before close & join.')
         pool.close()
-        pool.join()
+        pool.join() # 刪掉這行，主程序會不等子程序跑完就結束
     print('after pool with clause')
